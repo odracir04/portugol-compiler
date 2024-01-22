4558bcf5bddc53f4c5f1fd3fb842c1982a03f810
Ricardo Oliveira <rfo20112004@gmail.com>
2024-01-22 23:55:51 +0000

    implemented basic program in assembly :)

--- src/compiler.c
+++ src/compiler.c
@@ -6,9 +6,27 @@ extern int yyparse();
 extern int yylex();
 extern int yylineno;
 
+FILE* output;
+
+void create_data_section() {
+    char text[] = "section .data\n";
+    fwrite(text, sizeof text - 1, 1, output);
+}
+
+void create_text_section() {
+    char text[] = "section .text\nglobal _start\n\n_start:\n";
+    fwrite(text, sizeof text - 1, 1, output);
+}
+
+void terminate_program() {
+    char text[] = "\t\tMOV eax,1\n\t\tMOV ebx,1\n\t\tINT 80h\n";
+    fwrite(text, sizeof text - 1, 1, output);
+}
+
 int main(int argc, char** argv) {
     int retv;
     yyin = fopen(argv[1], "r");
+    output = fopen("test.asm", "w+");
 
     if (!yyin) {
         printf("FileError: Could not find file");
