904c3dabd2efe131983febe51aedce3ca7dd6be5
Ricardo Oliveira <rfo20112004@gmail.com>
2024-02-05 23:47:30 +0000

    modified lexer testing

--- test.sh
+++ test.sh
@@ -2,5 +2,11 @@
 
 flex src/lexer.lex
 bison -d src/parser.y
-gcc -Wall lex.yy.c parser.tab.c src/tests.c -o test_suite
-./test_suite
+gcc -Wall src/lexer_test.c -o lexer_test_suite
+
+echo "INITIATING LEXER TESTS.."
+for file in ./tests/*; do
+    if [ -f "$file" ]; then
+        ./lexer_test_suite $file
+    fi
+done    
