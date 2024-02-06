4622894cb26093e2710196c832ef13eec11a223d
Ricardo Oliveira <rfo20112004@gmail.com>
2024-02-06 22:46:07 +0000

    normalized grammar to cnf except math

--- src/compiler.c
+++ src/compiler.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "compiler.h"
 
 extern FILE* yyin;
 extern int yyparse();
 extern int yylex();
 extern int yylineno;
+
+ASTNode* createTokenNode(NodeType type, NodeValue value, ASTNode* left, ASTNode* right) {
+    ASTNode* node = (ASTNode*)malloc(sizeof(ASTNode));
+    node->type = type;
+    node->value = value;
+    node->left = left;
+    node->right = right;
+
+    printf("Node was created!\n");
+    return node;
+}
+
 
 int main(int argc, char** argv) {
     int retv;
--- src/compiler.h
+++ src/compiler.h
@@ -1,5 +1,26 @@
 #ifndef PORTUGOL_COMPILER_COMPILER_H
 #define PORTUGOL_COMPILER_COMPILER_H
 
+#include <stdbool.h>
+
+typedef enum {
+    TOKEN, INT, STRING, DOUBLE, BOOL
+} NodeType;
+
+typedef union NodeValue {
+    int intValue;
+    double doubleValue;
+    char* stringValue;
+    bool booleanValue;
+} NodeValue;
+
+typedef struct ASTNode {
+    NodeType type;
+    NodeValue value;
+    struct ASTNode* left;
+    struct ASTNode* right;
+} ASTNode;
+
+ASTNode* createTokenNode(NodeType type, NodeValue value, ASTNode* left, ASTNode* right);
 
 #endif //PORTUGOL_COMPILER_COMPILER_H
