41d45ca821f8e37e3d60e821c7e183b54ed5dd9e
Ricardo Oliveira <up202205353@edu.fe.up.pt>
2025-05-02 00:06:42 +0100

    clean slate

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-flex src/lexer.lex
-bison -d -Wcounterexamples src/parser.y
-clang -o compiler parser.tab.c lex.yy.c src/compiler.c src/structure.c src/write_statements.c `llvm-config --ldflags --system-libs --libs all` -stdlib=libc -fsanitize=address
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-./build.sh
-./compiler test.portugol
-clang -o prog module.bc
-./prog
